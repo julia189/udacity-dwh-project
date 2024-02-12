e8632dffbb1eb412e97818d2168ca317f285a806
Julia Hehl <julia.hehl@bmw.de>
2024-02-12 20:05:27 +0100

    Clean up

--- sql_queries.py
+++ sql_queries.py
@@ -19,82 +19,80 @@ time_table_drop = "DROP TABLE IF EXISTS time;"
 
 staging_events_table_create= ("""
 CREATE TABLE IF NOT EXISTS staging_events (
-        artist varchar,
-        auth varchar,
-        firstName varchar,
-        gender char,
-        iteminSession integer,
-        lastName varchar,
+        artist VARCHAR,
+        auth VARCHAR,
+        firstName VARCHAR,
+        gender CHAR,
+        iteminSession INTEGER,
+        lastName VARCHAR,
         length float,
-        level varchar,
-        location varchar,
-        method varchar,
-        page varchar,
-        registration long,
-        sessionId integer,
-        song varchar,
-        status integer,
-        ts long,
-        userAgent varchar,
-        userId integer                
+        level VARCHAR,
+        location VARCHAR,
+        method VARCHAR,
+        page VARCHAR,
+        registration BIGINT,
+        sessionId INTEGER,
+        song VARCHAR,
+        status INTEGER,
+        ts BIGINT,
+        userAgent VARCHAR,
+        userId INTEGER                
 )
 """)
 
 staging_songs_table_create = ("""
 CREATE TABLE IF NOT EXISTS staging_songs (
-        artist_id varchar,
-        artist_latitude float,
-        artist_location varchar,
-        artist_longitude float,
-        artist_name varchar,
-        duration float,
-        num_songs integer,
-        title varchar,
-        year integer
+        artist_id VARCHAR,
+        artist_latitude FLOAT,
+        artist_location VARCHAR,
+        artist_longitude FLOAT,
+        artist_name VARCHAR,
+        duration FLOAT,
+        num_songs INTEGER,
+        title VARCHAR,
+        year INTEGER
 )
 """)
 
 songplay_table_create = ("""
 CREATE TABLE IF NOT EXISTS songplays (
-                         songplay_id varchar(32) NOT NULL PRIMARY KEY,
-                         start_time date , 
-                         user_id varchar(32),
-                         level integer NOT NULL,
-                         song_id varchar(32),
-                         artist_id varchar(32)  ,
-                         session_id varchar(32) NOT NULL,
-                         location varchar(32) NOT NULL,
-                         user_agent varchar(32) NOT NULL
-
+                         songplay_id VARCHAR(32) NOT NULL PRIMARY KEY,
+                         start_time TIMESTAMP NOT NULL SORTKEY DISTKEY, 
+                         user_id VARCHAR(32),
+                         level INTEGER NOT NULL,
+                         song_id VARCHAR(32),
+                         artist_id VARCHAR(32)  ,
+                         session_id VARCHAR(32) NOT NULL,
+                         location VARCHAR(32) NOT NULL,
+                         user_agent VARCHAR(32) NOT NULL
 )
-                        
+DISTSTYLE KEY;                     
 """)
 
 user_table_create = ("""
 CREATE TABLE IF NOT EXISTS users (
-                     users_id varchar(32) NOT NULL PRIMARY KEY,
-                     first_name varchar(32) NOT NULL,
-                     last_name varchar(32) NOT NULL,
-                     gender varchar(5) NOT NULL,
-                     level integer NOT NULL 
-
+                     users_id VARCHAR(32) NOT NULL PRIMARY KEY SORTKEY,
+                     first_name VARCHAR(32) NOT NULL,
+                     last_name VARCHAR(32) NOT NULL,
+                     gender VARCHAR(5) NOT NULL,
+                     level INTEGER NOT NULL 
 );
 """)
 
 song_table_create = ("""
 CREATE TABLE IF NOT EXISTS songs (
-                    song_id varchar(32) NOT NULL,
-                    title varchar(32) NOT NULL,
-                    artist_id varchar(32) NOT NULL,
-                    year integer NOT NULL,
-                    duration integer NOT NULL -- #TODO: check if float
+                    song_id VARCHAR(32) NOT NULL PRIMARY KEY SORTKEY,
+                    title VARCHAR(32) NOT NULL,
+                    artist_id VARCHAR(32) NOT NULL,
+                    year INTEGER NOT NULL,
+                    duration INTEGER NOT NULL
 );
 """)
 
 artist_table_create = ("""
 CREATE TABLE IF NOT EXISTS artist (
-                       artist_id varchar(32) NOT NULL PRIMARY KEY,
-                       location varchar(32) NOT NULL,
+                       artist_id VARCHAR(32) NOT NULL PRIMARY KEY SORTKEY,
+                       location VARCHAR(32) NOT NULL,
                        latitude float NOT NULL,
                        longitude float NOT NULL
                   
@@ -103,23 +101,32 @@ CREATE TABLE IF NOT EXISTS artist (
 
 time_table_create = ("""
 CREATE TABLE IF NOT EXISTS time (
-                     start_time date NOT NULL PRIMARY KEY,
+                     start_time date NOT NULL PRIMARY KEY SORTKEY DISTKEY,
                      hour integer NOT NULL,
                      day integer NOT NULL,
                      week integer NOT NULL,
                      month integer NOT NULL,
                      year integer NOT NULL,
-                     weekday varchar(32) NOT NULL
+                     weekday VARCHAR(32) NOT NULL
 );
+DISTSTYLE KEY;
 """)
 
 # STAGING TABLES
 
 staging_events_copy = ("""
-""").format()
+COPY staging_events
+FROM {}
+IAM_ROLE {}
+FORMAT AS json {};               
+""").format(config['S3']['LOG_DATA'], config['IAM_ROLE']['ARN'], config['S3']['LOG_JSONPATH'])
 
 staging_songs_copy = ("""
-""").format()
+COPY staging_songs
+FROM {}
+IAM_ROLE {}
+FORMAT AS json 'auto';
+""").format(config['S3']['SONG_DATA'], config['IAM_ROLE']['ARN'], config['S3'][])
 
 # FINAL TABLES
 
