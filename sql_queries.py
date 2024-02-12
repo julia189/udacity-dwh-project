9c6ea624d488db5cd3416c6a36e8a51894dc759e
Julia Hehl <julia.hehl@bmw.de>
2024-02-12 18:31:52 +0100

    Fix error in sql statements

--- sql_queries.py
+++ sql_queries.py
@@ -7,13 +7,13 @@ config.read('dwh.cfg')
 
 # DROP TABLES
 
-staging_events_table_drop = "DROP TABLE staging_events IF EXISTS;"
-staging_songs_table_drop = "DROP TABLE staging_songs IF EXISTS;"
-songplay_table_drop = "DROP TABLE songplays IF EXISTS;"
-user_table_drop = "DROP TABLE users IF EXISTS;"
-song_table_drop = "DROP TABLE songs IF EXISTS;"
-artist_table_drop = "DROP TABLE artists IF EXISTS;"
-time_table_drop = "DROP TABLE time IF EXISTS;"
+staging_events_table_drop = "DROP TABLE IF EXISTS staging_events;"
+staging_songs_table_drop = "DROP TABLE IF EXISTS staging_songs;"
+songplay_table_drop = "DROP TABLE IF EXISTS songplays;"
+user_table_drop = "DROP TABLE IF EXISTS users;"
+song_table_drop = "DROP TABLE IF EXISTS songs;"
+artist_table_drop = "DROP TABLE IF EXISTS artists;"
+time_table_drop = "DROP TABLE IF EXISTS time;"
 
 # CREATE TABLES
 
