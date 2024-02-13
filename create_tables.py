b168c657e12ebb7878c5848e1c7cd10da065ae45
Julia Hehl <julia.hehl@bmw.de>
2024-02-13 13:43:17 +0100

    Adding docstring and commenting to python files

--- create_tables.py
+++ create_tables.py
@@ -4,27 +4,39 @@ from sql_queries import create_table_queries, drop_table_queries
 
 
 def drop_tables(cur, conn):
+    """
+    Executes each query in the drop_table_queries list and commits each transaction.
+    """
     for query in drop_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def create_tables(cur, conn):
+    """
+    Executes each query in the create_table_queries list and commits each transactions.
+    """
     for query in create_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def main():
+
+    # parsing the data warehouse configuration
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
+    # setting up the connection 
     conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
     cur = conn.cursor()
 
+    # dropping tables from schema
     drop_tables(cur, conn)
+    # creating tables in schema 
     create_tables(cur, conn)
 
+    # closing the dwh connection
     conn.close()
 
 
--- etl.py
+++ etl.py
@@ -4,27 +4,40 @@ from sql_queries import copy_table_queries, insert_table_queries
 
 
 def load_staging_tables(cur, conn):
+    """
+    Executes each query in copy_table_queries and commits each transaction.
+    """
     for query in copy_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def insert_tables(cur, conn):
+    """
+    Executes each query in insert_table_queries and commits each transaction.
+    """
     for query in insert_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def main():
+
+
+    # parsing the data warehouse configuration
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
+    # setting up the connection 
     conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
     cur = conn.cursor()
     
+    # loading data into staging tables
     load_staging_tables(cur, conn)
+    # inserting data into star schema tables
     insert_tables(cur, conn)
 
+    # closing dwh connection
     conn.close()
 
 
