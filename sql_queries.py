e532f6b67266ea492f19065f7e253d0779c609c6
Julia Hehl <julia.hehl@bmw.de>
2024-02-13 13:24:14 +0100

    Fix datatype problem

--- sql_queries.py
+++ sql_queries.py
@@ -97,6 +97,7 @@ SORTKEY (song_id);
 artist_table_create = ("""
 CREATE TABLE IF NOT EXISTS artists (
                        artist_id VARCHAR(32) NOT NULL PRIMARY KEY,
+                       artist_name VARCHAR,
                        location VARCHAR,
                        latitude VARCHAR,
                        longitude VARCHAR
@@ -230,7 +231,10 @@ time_table_insert = ("""
 
 # QUERY LISTS
 
-create_table_queries = [staging_events_table_create, staging_songs_table_create, songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
-drop_table_queries = [staging_events_table_drop, staging_songs_table_drop, songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
+create_table_queries = [ #staging_events_table_create,
+                         #staging_songs_table_create,
+                        songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
+drop_table_queries = [#staging_events_table_drop, staging_songs_table_drop, 
+    songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
 copy_table_queries = [staging_events_copy, staging_songs_copy]
 insert_table_queries = [songplay_table_insert, user_table_insert, song_table_insert, artist_table_insert, time_table_insert]
